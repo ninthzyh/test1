99d6dcd997534adc1b220c6efd96f7a8cb6d00d0
chenchen <cc.2008.com@163.com>
2020-07-15 16:09:53 +0800

    all

--- src/pages/society/widget/Index.js
+++ src/pages/society/widget/Index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import SocietyStyle from '../Society.module.scss'
 
+import OneMap from '../mapController';
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( <></> );
+    render() {
+        return (
+            <div className={SocietyStyle.container}>
+                <OneMap></OneMap>
+                <div className={SocietyStyle.page}>
+                    <div>
+                        <div className={SocietyStyle.item}>1</div>
+                        <div className={SocietyStyle.item}>
+
+                        </div>
+                        <div className={SocietyStyle.item}>3</div>
+                    </div>
+                    <div>
+                        <div className={SocietyStyle.item}>4</div>
+                        <div className={SocietyStyle.item}>5</div>
+                        <div className={SocietyStyle.item}>6</div>
+                    </div>
+                </div>
+            </div>);
+
     }
 }
- 
+
 export default Index;
