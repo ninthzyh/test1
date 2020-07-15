aacf1d38a50b6cc02d1c2446a6977924e42411ce
yangchen <462428585@qq.com>
2020-07-15 18:29:52 +0800

    'one'

--- src/pages/affairs/widget/Index.js
+++ src/pages/affairs/widget/Index.js
@@ -6,12 +6,29 @@ import One from './One.js'
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            show: 'one'
+        }
     }
     render() {
+        const {show} = this.state;
         return (
             <div className={AffairsStyle.container}>
                 <OneMap></OneMap>
+                {show === 'one' ? <div className={AffairsStyle.page}>
+                    <div className={AffairsStyle.wrapper}>
+                        <div className={AffairsStyle.item}><One/></div>
+                        <div className={AffairsStyle.item}>
+                            <Two />
+                        </div>
+                        <div className={AffairsStyle.item}>3</div>
+                    </div>
+                    <div className={AffairsStyle.wrapper}>
+                        <div className={AffairsStyle.itemRight}>1</div>
+                        <div className={AffairsStyle.itemRight}>2</div>
+                        <div className={AffairsStyle.itemRight}>3</div>
+                    </div>
+                </div>:
                 <div className={AffairsStyle.page}>
                     <div className={AffairsStyle.wrapper}>
                         <div className={AffairsStyle.item}><One/></div>
@@ -25,7 +42,7 @@ class Index extends Component {
                         <div className={AffairsStyle.itemRight}>2</div>
                         <div className={AffairsStyle.itemRight}>3</div>
                     </div>
-                </div>
+                </div>}
             </div>);
     }
 }
