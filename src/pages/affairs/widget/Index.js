ba62c2327b428ac90043e638115cba1d2adc277f
ninthzyh <zhengyuhe0625@163.com>
2020-07-16 16:08:27 +0800

    update

--- src/pages/affairs/widget/Index.js
+++ src/pages/affairs/widget/Index.js
@@ -39,7 +39,7 @@ class Index extends Component {
         const { show } = this.state;
         return (
             <div className={AffairsStyle.container}>
-                <OneMap/>
+                {/* <OneMap/> */}
                 <div style={!show ? {display: 'none'} : {}} className={this.getClassName(!show,'Left')}>
                     <div className={AffairsStyle.item}><One/></div>
                     <div className={AffairsStyle.item}><Two /></div>
