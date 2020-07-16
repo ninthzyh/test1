7b9e5f8e7590c2d208038c2e214f355e8a93efa2
ninthzyh <zhengyuhe0625@163.com>
2020-07-16 10:41:02 +0800

    eight&nine

--- src/pages/affairs/widget/Index.js
+++ src/pages/affairs/widget/Index.js
@@ -12,11 +12,12 @@ import Eight from './Eight';
 import Four from './Four/Four.js'
 import FourChart from './Four/FourChart/FourChart.js'
 import Eleven from './humanSocialBureau/Eleven';
+import Nine from './Nine';
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true,
+            show: false,
         }
     }
     componentDidMount() {
@@ -24,11 +25,11 @@ class Index extends Component {
     }
 
     showChange = () => {
-        setTimeout(()=>{
-            this.setState({
-                show: !this.state.show
-            }, this.showChange)
-        },5000)
+        // setTimeout(()=>{
+        //     this.setState({
+        //         show: !this.state.show
+        //     }, this.showChange)
+        // },5000)
     };
 
     getOneClassName = (show) => !show ? 'animate__animated animate__backOutLeft' : 'animate__animated animate__backInLeft';
@@ -37,7 +38,7 @@ class Index extends Component {
     render() {
         const { show } = this.state;
         return (
-            <div className={AffairsStyle.page}>
+            <div className={AffairsStyle.container}>
                 <OneMap></OneMap>
                 <div style={!show ? {display: 'none'} : {}} className={`${AffairsStyle.leftWrapper} ${this.getOneClassName(show)}`}>
                     <div className={AffairsStyle.item}><One/></div>
@@ -52,7 +53,7 @@ class Index extends Component {
                 <div style={show ? {display: 'none'} : {}} className={`${AffairsStyle.leftWrapper} ${this.getTwoClassName(show)}`}>
                     <div className={AffairsStyle.item}><Seven/></div>
                     <div className={AffairsStyle.item}><Eight /></div>
-                    <div className={AffairsStyle.item}>3</div>
+                    <div className={AffairsStyle.item}><Nine /></div>
                 </div>
                 <div style={show ? {display: 'none'} : {}} className={`${AffairsStyle.rightWrapper} ${this.getTwoClassName(show)}`}>
                     <div className={AffairsStyle.itemRight}><FourChart/></div>
