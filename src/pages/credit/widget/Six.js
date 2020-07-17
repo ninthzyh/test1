3a93870567506c338119f02d6ac598c601fdb063
chen1218chen <cc.2008.com@163.com>
2020-07-17 15:55:02 +0800

    样式调整

--- src/pages/credit/widget/Six.js
+++ src/pages/credit/widget/Six.js
@@ -9,14 +9,17 @@ export default class extends Component {
     render() {
         return (
             <div className={CreditStyle.six}>
-                <ChartHeader title='信用动态'/>
+                <ChartHeader title='信用动态' />
                 <div className={CreditStyle.bg}>
-                    <img src="img/credit/news.png" width='70%' alt=""/>
+                    <img src="img/credit/news.png" width='80%' alt="" />
                 </div>
                 <div className={CreditStyle.marquee}>
-                    <marquee direction='up' scrollamount='1' height='20%'>
-                        <p>7月15日，全市文化旅游大会召开，深入学习贯彻习近平总书记视察河 南重要讲话和关于文化旅游融合发展的重要指示精神，认真贯彻省委十 届十一次全会暨省委工作会议精神，全面落实全省文化旅游大会精神， 动员全市上下坚定文化自信、抢抓战略机遇，发挥自身优势、主动担当</p>
-                    </marquee>
+                    <div>
+                        <marquee direction='up' scrollamount='1' height='20%' width="100%">
+                            <p>7月15日，全市文化旅游大会召开，深入学习贯彻习近平总书记视察河 南重要讲话和关于文化旅游融合发展的重要指示精神，认真贯彻省委十 届十一次全会暨省委工作会议精神，全面落实全省文化旅游大会精神， 动员全市上下坚定文化自信、抢抓战略机遇，发挥自身优势、主动担当</p>
+                        </marquee>
+
+                    </div>
                 </div>
 
             </div>);
