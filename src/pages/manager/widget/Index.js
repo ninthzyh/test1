e13c35d0d1a650a7435a42971f429ccc4b1c28ca
yangchen <462428585@qq.com>
2020-07-17 10:15:23 +0800

    '文件'

--- src/pages/manager/widget/Index.js
+++ src/pages/manager/widget/Index.js
@@ -1,39 +1,56 @@
 import React from 'react';
 import ManagerStyle from '../Manager.module.scss';
-import Six from "./Six";
 import OneMap from '../mapController';
 import One from './One';
-import FiveChart from './fiveChart/FiveChart';
 import Two from './Two/Two';
-
-import Four from './four/Four'
 import Three from './Three';
-import { T } from 'antd/lib/upload/utils';
+import Four from './four/Four'
+import FiveChart from './fiveChart/FiveChart';
+import Six from "./Six";
 class Manager extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            show:false
         }
     }
+    componentDidMount() {
+        this.showChange();
+    }
+
+    showChange = () => {
+        setTimeout(()=>{
+            this.setState({
+                show: !this.state.show
+            }, this.showChange)
+        },5000)
+    };
+    getClassName = (show,position) => `${ManagerStyle[`${position.toLowerCase()}Wrapper`]} ${show ? `animate__animated animate__backOut${position}` : `animate__animated animate__backIn${position}`}`;
+
     render() {
+        const { show } = this.state;
         return (
             <div className={ManagerStyle.container}>
-                {/* <OneMap></OneMap> */}
-                <div className={ManagerStyle.leftWrapper}>
+                {/* <OneMap/> */}
+                <div style={!show ? {display: 'none'} : {}} className={this.getClassName(!show,'Left')}>
                     <div className={ManagerStyle.item}><One /></div>
                     <div className={ManagerStyle.item}><Two /></div>
                     <div className={ManagerStyle.item}><Three/></div>
-                    <div className={ManagerStyle.item}>
-
-                    </div>
-                    <div className={ManagerStyle.item}></div>
                 </div>
-                <div  className={ManagerStyle.rightWrapper}>
-                    <div className={ManagerStyle.itemRight}><Four/></div>
+                <div style={!show ? {display: 'none'} : {}} className={this.getClassName(!show,'Right')}>
+                    <div className={ManagerStyle.itemRight}><Four /></div>
                     <div className={ManagerStyle.itemRight}><FiveChart /></div>
                     <div className={ManagerStyle.itemRight}><Six /></div>
-                    <div className={ManagerStyle.itemRight}></div>
-                    <div className={ManagerStyle.itemRight}></div>
+                </div>
+                <div style={show ? {display: 'none'} : {}} className={this.getClassName(show,'Left')}>
+                    <div className={ManagerStyle.item}><One /></div>
+                    <div className={ManagerStyle.item}><Two /></div>
+                    <div className={ManagerStyle.item}><Three/></div>
+                </div>
+                <div style={show ? {display: 'none'} : {}} className={this.getClassName(show,'Right')}>
+                    <div className={ManagerStyle.itemRight}><Four /></div>
+                    <div className={ManagerStyle.itemRight}><FiveChart /></div>
+                    <div className={ManagerStyle.itemRight}><Six /></div>
                 </div>
             </div>);
 
