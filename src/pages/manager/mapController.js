6842f59505bd314c3d4ed98b42e36ccbd566d5e1
张海亮 <xytc-zh@163.com>
2020-07-18 18:37:34 +0800

    add wangge

--- src/pages/manager/mapController.js
+++ src/pages/manager/mapController.js
@@ -2,15 +2,16 @@
 import React, {Component} from 'react';
 // import {render} from 'react-dom';
 import {StaticMap} from 'react-map-gl';
-import mapboxgl, { DoubleClickZoomHandler }  from 'mapbox-gl';
+import mapboxgl  from 'mapbox-gl';
 import MapboxLanguage from '@mapbox/mapbox-gl-language';
 import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
 import DeckGL, {FlyToInterpolator} from 'deck.gl';
 import {GeoJsonLayer, PathLayer} from '@deck.gl/layers';
 import {H3HexagonLayer} from '@deck.gl/geo-layers';
 import PolylineLayer from 'components/polyline-layer/polyline-layer';
-import ArcLayerExt from 'components/arc-layer/arc-layer-ext';
-import ScanLayer from 'components/scan-layer/scan-layer';
+import { Popup } from 'react-map-gl';
+// import ArcLayerExt from 'components/arc-layer/arc-layer-ext';
+// import ScanLayer from 'components/scan-layer/scan-layer';
 import cityData from 'assets/json/PuYang_City.geojson';
 import roadData from 'assets/json/PuYang_Roads.json';
 import buildData from 'assets/json/PuYang_Buildings.geojson';
@@ -18,6 +19,9 @@ import countyData from 'assets/json/PuYang_County.geojson';
 import arcData from 'assets/json/PuYang_arc.json';
 import hexagonData from 'assets/json/PuYangCity_WangGe.json';
 import pathImg from 'assets/images/path.png';
+// import colorImg from 'img/color.png';
+// import noiseImg from 'img/depth.png';
+import './managerPopup.scss';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = 'pk.eyJ1IjoieHl0Y3poIiwiYSI6ImNrOWNzZ3ZidDA3bnMzbGxteng1bWc0OWIifQ.QKsCoDJL6Qg8gjQkK3VCoQ'; // eslint-disable-line
@@ -31,12 +35,12 @@ const ambientLight = new AmbientLight({
 const pointLight = new PointLight({
   color: [255, 255, 255],
   intensity: 2.0,
-  position: [114.9125, 35.9860, 5000]
+  position: [114.9125, 35.9860, 8000]
 });
 const pointLightRight = new PointLight({
   color: [255, 255, 255],
   intensity: 2.0,
-  position: [115.1525, 35.8060, 5000]
+  position: [115.1525, 35.8060, 8000]
 });
 
 
@@ -109,6 +113,15 @@ const viewStates = [
     bearing: 45,
     transitionDuration: 5000,
     transitionInterpolator: new FlyToInterpolator()
+  },
+  {
+    longitude: 115.0236,
+    latitude: 35.7141,
+    zoom: 14,
+    pitch: 60,
+    bearing: 55,
+    transitionDuration: 5000,
+    transitionInterpolator: new FlyToInterpolator()
   }
 ];
 
@@ -166,15 +179,14 @@ export default class OneMap extends Component {
         data: hexagonData,
         pickable: true,
         wireframe: false,
-        filled: true,
+        filled: false,
         extruded: false,
         elevationScale: 2,
         getHexagon: d => d.hex,
-        getFillColor: d => [0, (1 - d.count / 500) * 220, 255,255/2],
+        getFillColor: d => [0, (1 - d.count / 500) * 220, 255,255],
         getElevation: d => d.count,
         getLineColor: d => [85,245,255],
-        getLineWidth: 15,
-        // opacity: 1
+        getLineWidth: 5
       }),
       new PathLayer({
         id: 'pathlayer',
@@ -232,13 +244,12 @@ export default class OneMap extends Component {
         //       {position: [115.015, 35.7050], color: [200, 0, 0], radius: 1500}
         //     ],
         //     getPosition: d => d.position,
-        //     image: imgUrl + 'color.png',
-        //     imageNoise: imgUrl + 'depth.png',
+        //     image: colorImg,
+        //     imageNoise: noiseImg,
         //     getRadius: d => d.radius,
         //     speed: 6,
         //     getBlendColor: [0, 255, 0]
         // }),
-
         // new GeoJsonLayer({
         //   id: 'city-layer',
         //   data:this.state.cityData,
@@ -267,7 +278,8 @@ export default class OneMap extends Component {
     ];
   }
   _onLoad(e) {
-    console.dir(e);
+    let box = document.getElementsByClassName('mapboxgl-map')[0].parentNode
+    box.style.zIndex = ''
     map = e.target;
     mapboxgl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.1/mapbox-gl-rtl-text.js');
     e.target.addControl(new MapboxLanguage({
@@ -299,7 +311,78 @@ export default class OneMap extends Component {
           preventStyleDiffing={true}
           mapboxApiAccessToken={MAPBOX_TOKEN}
           onLoad={this._onLoad}
-        />
+        >
+        <Popup className={'manager managerPopup1'}
+              longitude={115.0236}
+              latitude={35.7141}
+              altitude={0}
+              anchor = {'bottom'}
+              dynamicPosition = {false}
+              sortByDepth = {true}
+              closeButton={false}
+              visible={true}
+            >
+              <div className='managerPopup1 font'>1</div>
+              <div className='managerPopup1 title'>网格统计情况</div>
+              <div className='managerPopup1 static'>5</div>
+              <div className='managerPopup1 static'>3</div>
+              <div className='managerPopup1 static'>1</div>
+              <div className='managerPopup1 static'>8</div>
+              <div className='managerPopup1 static'>10</div>
+            </Popup>
+            <Popup className={'manager managerPopup1'}
+              longitude={115.0124}
+              latitude={35.7043}
+              altitude={0}
+              anchor = {'bottom'}
+              dynamicPosition = {false}
+              sortByDepth = {true}
+              closeButton={false}
+              visible={true}
+            >
+              <div className='managerPopup1 font'>2</div>
+              <div className='managerPopup1 title'>网格统计情况</div>
+              <div className='managerPopup1 static'>4</div>
+              <div className='managerPopup1 static'>5</div>
+              <div className='managerPopup1 static'>1</div>
+              <div className='managerPopup1 static'>10</div>
+              <div className='managerPopup1 static'>9</div>
+            </Popup>
+            <Popup className={'manager managerPopup1'}
+              longitude={115.0379}
+              latitude={35.7174}
+              altitude={0}
+              anchor = {'bottom'}
+              dynamicPosition = {false}
+              sortByDepth = {true}
+              closeButton={false}
+              visible={true}
+            >
+              <div className='managerPopup1 font'>3</div>
+              <div className='managerPopup1 title'>网格统计情况</div>
+              <div className='managerPopup1 static'>6</div>
+              <div className='managerPopup1 static'>4</div>
+              <div className='managerPopup1 static'>1</div>
+              <div className='managerPopup1 static'>7</div>
+              <div className='managerPopup1 static'>9</div>
+            </Popup>
+            <Popup className={'manager managerPopup2'}
+              longitude={115.0159}
+              latitude={35.7273}
+              altitude={0}
+              anchor = {'bottom'}
+              dynamicPosition = {false}
+              sortByDepth = {true}
+              closeButton={false}
+              visible={true}
+            >
+              <div className='managerPopup2 title'>抢劫治安事件</div>
+              <div className="managerPopup2 body">
+                <div className='managerPopup2 content'>濮阳县解放大道建业城小区附近</div>
+                <div className='managerPopup2 content'>2020-07-07 11:34:20</div>
+              </div>
+            </Popup>
+            </StaticMap>
       </DeckGL>
     );
   }
