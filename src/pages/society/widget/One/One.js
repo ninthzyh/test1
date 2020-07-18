eaa696fc5a5de612c22ad220018eafa101530e2b
Taddy <2437266454@qq.com>
2020-07-18 11:13:30 +0800

    修改

---
+++ src/pages/society/widget/One/One.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactEcharts from "echarts-for-react";
+import echarts from 'echarts/lib/echarts';
+import styles from './One.module.scss';
+class One extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (<div className={styles.OnePage}>
+
+        </div>);
+    }
+}
+
+export default One;
