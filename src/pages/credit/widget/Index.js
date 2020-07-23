0590c952955ebff0789292d56c948b022ac75e74
yangchen <462428585@qq.com>
2020-07-23 15:08:51 +0800

    地图

--- src/pages/credit/widget/Index.js
+++ src/pages/credit/widget/Index.js
@@ -15,7 +15,7 @@ class Index extends Component {
     render() {
         return (
             <div className={CreditStyle.container}>
-                {/*<OneMap></OneMap>*/}
+                <OneMap></OneMap>
                 <div className={` ${CreditStyle.leftWrapper}`}>
                     <div  className={CreditStyle.item}>
                         <One />
