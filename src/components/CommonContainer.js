1285dfd26651874bfee8fab345739b695e06499f
ninthzyh <zhengyuhe0625@163.com>
2020-07-24 17:10:10 +0800

    update

--- src/components/CommonContainer.js
+++ src/components/CommonContainer.js
@@ -11,7 +11,7 @@ export default class CommonContainer extends Component {
     }
 
     componentDidMount() {
-        if(this.props.children > 6){
+        if(this.props.children.length > 6){
             this.showChange();
         }
         
