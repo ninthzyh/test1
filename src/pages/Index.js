fcd86d9f3e45398ee5d10b2d33f64a1beeb35dd8
yangchen <462428585@qq.com>
2020-08-10 08:37:17 +0800

    yansee

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -17,7 +17,7 @@ export default class Home extends React.Component {
 
   render() {
     const route = this.state.route;
-    return <Layout className="layout" style={{ height: '100%',backgroundColor:'#000000'}}>
+    return <Layout className="layout" style={{ height: '100%',backgroundColor:'#3F5F6D'}}>
       <Header className='animate__animated animate__backInDown' hasMenu={true} />
       <Content style={{ height: '100%' }} id="Content">
         {renderRoutes(route.children)}
